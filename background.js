fb9252f24e410fac35f9b738a140e4554fd749b0
Takyoung Woo <takyoung.w@gmail.com>
2022-04-05 17:43:33 +0200

    remove the correctly guessed word and gshow a new word

---
+++ background.js
@@ -0,0 +1,11 @@
+
+
+// function setup() {
+//     let image;
+//     createCanvas(1600,1000)
+//     img = loadImage('/src/beach-background.gif')
+// }
+
+// function draw(){
+//     image(img, 0, 0)
+// }
